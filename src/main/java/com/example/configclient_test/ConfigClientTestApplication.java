1307b1edc3c2b18762c0afedf45f284ce8245bf2
yokxim <yooksi53@gmail.com>
2025-02-10 14:30:37 +0900

    Initial commit

---
+++ src/main/java/com/example/configclient_test/ConfigClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.configclient_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/configclient_test/ConfigClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configclient_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
